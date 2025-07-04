dc533d6c4e9e706ac9d987264d2c25a4cb7b1afa
ahmed hassan <uyu365222@gmail.com>
2025-07-04 15:52:30 +0300

    finsh reports

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Contract;
+use App\Models\Inquiry;
+use App\Models\Customer;
+
+class ReportController extends Controller
+{
+    // Orders & Clients
+
+}
